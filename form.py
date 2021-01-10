720e396943f00a0ac5ddc7193fb604221685ef73
Jordan Godau <jgodau@my.bcit.ca>
2021-01-09 17:39:35 -0800

    added form

--- form.py
+++ form.py
@@ -1,11 +1,15 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField
 from wtforms.validators import InputRequired
-import get_top_artists
+from get_top_artists import get_top_artists
 
 
 def validate_artist(artist_name):
 
+    """Validate artist name against list of artists."""
+
+    if artist_name not in get_top_artists():
+        raise Val
 
 
 class WordSearchForm(FlaskForm):
@@ -13,3 +17,9 @@ class WordSearchForm(FlaskForm):
     """Create the form."""
 
     word_search = StringField('search', )
+
+    def validate_artist(FlaskForm, artist_name):
+        """Validate artist name against list of artists."""
+
+        if artist_name not in get_top_artists():
+            raise Valida
