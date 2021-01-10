3e05a69324e4a16323e7a863c75921a8929117cb
Jordan Godau <jgodau@my.bcit.ca>
2021-01-09 21:44:07 -0800

    changed pie chart

--- make_pie_chart.py
+++ make_pie_chart.py
@@ -1,12 +1,26 @@
 import matplotlib.pyplot as plt
 
+
 def make_pie_chart(artist_word_counts):
 
+    # get the labels
     labels = [pair[0] for pair in artist_word_counts]
+    # get the counts
     counts = [pair[1] for pair in artist_word_counts]
-    explode = (0.1, 0, 0, 0, 0)
+    # break off the highest count
+    # explode = (0.5, 0, 0, 0, 0)
 
+    # make the chart
     figure, axis = plt.subplots()
-    axis.pie(sizes, explode=explode, labels=labels, autopct='%1.1f%%')
+    axis.pie(counts, shadow=True, labels=labels, autopct='%1.1f%%')
     axis.axis('equal')
-    plt.show()
+    plt.tight_layout()
+    plt.savefig(r"static/plot.png")
+
+make_pie_chart([
+    ("kanye", 65),
+    ("taylor swift", 25),
+    ("drake", 16),
+    ("Ariana Grande", 8),
+    ("Pink", 4),
+])
