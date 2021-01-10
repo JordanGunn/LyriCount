b263dc2dd5682e820c75fce80f4ca0074edc3e79
Jordan Godau <jgodau@my.bcit.ca>
2021-01-10 01:45:25 -0800

    minor changes to pie chart and template

--- app.py
+++ app.py
@@ -13,10 +13,7 @@ from make_pie_chart import make_pie_chart
 app = Flask(__name__)
 
 app.config['SECRET_KEY'] = 'kjhsdfkgjh345toisufg980sd7g2l34khasd987asdfk'
-
-# @app.route('/')
-# def home():
-#     return render_template("base.html")
+app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
 
 
 @app.route("/", methods=["GET", "POST"])
@@ -27,6 +24,15 @@ def search():
         make_pie_chart(artist_used_most(query, open_file()))
         return render_template("base.html", data=query, form=form)
     return render_template("base.html", form=form)
+
+
+@app.after_request
+def add_header(response):
+    # response.cache_control.no_store = True
+    response.headers['Cache-Control'] = 'no-store, no-cache, must-revalidate, post-check=0, pre-check=0, max-age=0'
+    response.headers['Pragma'] = 'no-cache'
+    response.headers['Expires'] = '-1'
+    return response
 
 
 if __name__ == '__main__':
