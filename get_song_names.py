efca86765f7b0c6d26908b079fa7db48c08004aa
nickluong97@outlook.com <nickluong97@outlook.com>
2021-01-09 17:09:11 -0800

    made changes to get_artist_songs(), generated a json for song names

--- get_song_names.py
+++ get_song_names.py
@@ -1,4 +1,5 @@
 import requests
+import json
 
 
 def get_song_names():
@@ -9,28 +10,28 @@ def get_song_names():
     return thing
 
 
-def parse_artist_name(artist_name: str) -> str:
+def parse_artist_name(artist_name):
+    pass
 
-    """
-    Parse artist name for url insertion.
 
-    :param artist_name:
-    :return:
-    """
-
-    split_artist_name = artist_name.split(" ")
-    if len(split_artist_name) > 1:
-        parsed_artist_name = "+".join(split_artist_name)
-        return parsed_artist_name
-    else:
-        return artist_name
+"guns+and+roses"
 
 
 def get_artist_songs():
     url = 'https://itunes.apple.com/search?term=guns+and+roses&entity=musicTrack&limit=200'
     data = requests.get(url)
     response = data.json()
-    songs = response["results"]
+    artist = response["results"]
+    songs = [song["trackName"] for song in artist]
+    artist_songs = {"artistName": songs}
     filename = "artist_songs.json"
     with open(filename, "w+") as file_object:
-        json.dump(songs, file_object)
+        json.dump(artist_songs, file_object)
+
+
+def main():
+    get_artist_songs()
+
+
+if __name__ == '__main__':
+    main()
