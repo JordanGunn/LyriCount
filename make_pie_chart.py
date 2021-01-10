68396707c642c4b8fd5977a8892a9dc6e77fd212
nickluong97@outlook.com <nickluong97@outlook.com>
2021-01-10 11:56:35 -0800

    more minor css, add heading, change plot font and background, fixed plot padding.

--- make_pie_chart.py
+++ make_pie_chart.py
@@ -1,6 +1,7 @@
 import matplotlib.pyplot as plt
 from artist_used_most import artist_used_most, open_file
 from PIL import Image
+import matplotlib
 
 wc = artist_used_most("hello", open_file())
 
@@ -19,15 +20,16 @@ def make_pie_chart(artist_counts):
 
     # make the chart
     if sum(counts) > 0:
+        matplotlib.rcParams['text.color'] = 'white'
         figure, axis = plt.subplots()
         axis.pie(counts, labels=labels, autopct='%1.1f%%', normalize=True)
         axis.axis('equal')
         plt.rc('font', family='Franklin Gothic Book')
-        plt.tight_layout()
-        plt.savefig(r"static/plot.png")
+        plt.tight_layout(pad=6)
+        plt.savefig(r"static/plot.png", facecolor="#172B41")
         plot_img = Image.open(r"static/plot.png")
         plot_img = plot_img.resize((750, 550))
-        plot_img.save(r"static/plot.png", 'PNG')
+        plot_img.save(r"static/plot.png", 'PNG', facecolor="#172B41")
     else:
         image = Image.open(r"./static/no_results_found.png")
         image = image.resize((640, 480))
