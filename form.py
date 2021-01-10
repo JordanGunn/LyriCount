87335cecd9fcc7d546769263ba7e0b2c71fcbeaa
Jordan Godau <jgodau@my.bcit.ca>
2021-01-09 17:38:45 -0800

    Updated get_top_artists with new url

---
+++ form.py
@@ -0,0 +1,15 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField
+from wtforms.validators import InputRequired
+import get_top_artists
+
+
+def validate_artist(artist_name):
+
+
+
+class WordSearchForm(FlaskForm):
+
+    """Create the form."""
+
+    word_search = StringField('search', )
--- get_top_artists.py
+++ get_top_artists.py
@@ -10,24 +10,28 @@ def TOP_URL():
     Location of top 200 artists from rolling stone.
     """
 
-    return r"https://www.rollingstone.com/charts/artists/"
+    return r"https://www.billboard.com/charts/artist-100"
 
 
-def get_top_artists(url):
+def get_top_artists():
 
     """
     Submit get request to server and get top 200 artists.
 
-    :param url:     Url location of top artists.
     :return:        List of artists.
     """
 
-    with requests.get(url) as top200:
+    with requests.get(TOP_URL()) as top200:
         # Create BeautifulSoup object from
         data = BeautifulSoup(top200.text, "html.parser")
         # get divs by class
-        divs = data.findAll("div", {"class": "c-chart__table--title"})
+        divs = data.findAll("div", {"class": "item-details__title"})
         # get the artist tags in the div
-        artists = [div.findAll("p")[0].text for div in divs]
+        artists = [div.text for div in divs]
 
     return artists
+
+
+if __name__ == "__main__":
+
+    print(get_top_artists())
